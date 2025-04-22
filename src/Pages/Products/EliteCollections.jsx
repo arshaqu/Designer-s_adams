d501f90cb0b533498b06e4b206afd13a5fa230f4
arshaqu <muhammedarshaque1999@gmail.com>
2025-04-22 13:13:39 +0530

    Elite banner updated

--- src/Pages/Products/EliteCollections.jsx
+++ src/Pages/Products/EliteCollections.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import AdhamsWhite from '../../Assets/whitered.png';
-import CladdingBanner from '../../Assets/Products/Banners/Wpc exterior.png';
+import EliteBanner from '../../Assets/Elite/eliteBg.jpg';
 
 import Image1 from '../../Assets/Elite/Image1.png'
 import Image2 from '../../Assets/Elite/Image2.png'
@@ -94,14 +94,14 @@ function EliteCollections() {
       <div className="w-full min-h-screen bg-gray-800">
         <Navbar />
         <div className="flex flex-col items-center montserrat">
-          {/* <div className="relative">
-            <img src={CladdingBanner} alt="Company Banner" className="w-auto h-auto" />
+          <div className="relative">
+            <img src={EliteBanner} alt="Company Banner" className="w-auto h-auto" />
             <div className="absolute inset-0 flex items-center justify-center mt-40 md:ml-60 ml-40">
               <div className="relative w-full md:ml-[596px] md:mt-[550px] ">
                 <div style={{ overflow: 'hidden' }}>
                   <h1 className="text-white text-xs sm:text-sm md:text-lg lg:text-2xl montserrat font-bold bg-opacity-45 bg-gray-800 px-4 sm:px-5 md:px-6 py-4 sm:py-3 md:py-5 w-full">
-                    <span className="text-xxs md:text-lg">Co-extrusion|Non brushed<br /></span>
-                    EXTERIOR CLADDINGS
+                    <span className="text-xxs md:text-lg">WPVC INTERIOR WALL PANELS<br /></span>
+                    PREMIUM COLLECTIONS
                   </h1>
                 </div>
                 <div style={{ backgroundColor: '#eb4c34' }} className="absolute mt-1 bottom-0 left-0 w-full h-3 md:h-6"></div>
@@ -112,7 +112,7 @@ function EliteCollections() {
               alt="Small Icon"
               className="absolute bottom-[-12px] md:bottom-8 left-1 md:left-12 w-24 h-16 m-5 md:w-64 md:h-auto object-contain"
             />
-          </div> */}
+          </div>
   
           {/* Product Grid */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10 w-full max-w-6xl px-4">
